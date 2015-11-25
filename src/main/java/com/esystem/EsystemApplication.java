0dbad84326458ad05306c9871d12e774bd6bfc9a
Brian Momongan <bmomonganfpt@gmail.com>
2015-11-25 18:12:41 +0800

    first commit

---
+++ src/main/java/com/esystem/EsystemApplication.java
@@ -0,0 +1,12 @@
+package com.esystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsystemApplication.class, args);
+    }
+}
